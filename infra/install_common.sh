3f84557a31ce731f96d481bdf681e84fe440f669
ynigoreyes <ynigoreyes@gmail.com>
2019-08-13 03:15:03 -0500

    moving to azure resource manager

--- infra/install_common.sh
+++ infra/install_common.sh
@@ -37,3 +37,49 @@ if [ "$(uname)" == "Darwin" ]; then
     open https://aka.ms/installazurecliwindows
     open https://nodejs.org/dist/v10.16.0/node-v10.16.0-x64.msi
 fi
+
+cd ~
+
+# Prerequisites
+if [ "$(uname)" == "Darwin" ]; then
+    brew install jq
+    # For Linux
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sudo apt-get install --assume-yes jq
+fi
+
+# Get URLs for most recent versions
+# For OS-X
+if [ "$(uname)" == "Darwin" ]; then
+    terraform_url=$(curl https://releases.hashicorp.com/index.json | jq '{terraform}' | egrep "darwin.*64" | sort --version-sort -r | head -1 | awk -F[\"] '{print $4}')
+    # For Linux
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    terraform_url=$(curl https://releases.hashicorp.com/index.json | jq '{terraform}' | egrep "linux.*amd64" | sort --version-sort -r | head -1 | awk -F[\"] '{print $4}')
+fi
+
+# Create a move into directory.
+cd
+mkdir terraform && cd $_
+
+# Download Terraform. URI: https://www.terraform.io/downloads.html
+echo "Downloading $terraform_url."
+curl -o terraform.zip $terraform_url
+# Unzip and install
+unzip terraform.zip
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo '
+  # Terraform Paths.
+  export PATH=~/terraform/:$PATH
+    ' >>~/.bash_profile
+    
+    source ~/.bash_profile
+    # For Linux
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo '
+  # Terraform Paths.
+  export PATH=~/terraform/:$PATH
+    ' >>~/.bashrc
+    
+    source ~/.bashrc
+fi
---
+++ infra/remove-resources.sh
@@ -0,0 +1,3 @@
+az group delete -n acm
+az group delete -n MC_acm_acm-aks_westus2 -y
+az group delete -n NetworkWatcherRG -y
