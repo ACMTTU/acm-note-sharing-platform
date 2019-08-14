ddc973200d5127716191e64a668911961ad17a22
ynigoreyes <ynigoreyes@gmail.com>
2019-08-14 17:34:53 -0500

    testing out raw docker login script

--- src/AuthApplication/Scripts/docker-build-push.sh
+++ src/AuthApplication/Scripts/docker-build-push.sh
@@ -1,5 +1,3 @@
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD acmregistry2019.azurecr.io
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker build $DIR/../. -t acmregistry2019.azurecr.io/authapplication:stable
