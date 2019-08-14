4f147b6e3f4fa47a16bf0ef74a63f8eb21043272
ynigoreyes <ynigoreyes@gmail.com>
2019-08-14 10:29:43 -0500

    adding working applications to be deployed

---
+++ src/AuthApplication/Scripts/docker-build-push.sh
@@ -0,0 +1,6 @@
+docker login acmregistry2019.azurecr.io -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build $DIR/../. -t acmregistry2019.azurecr.io/authapplication:stable
+docker push acmregistry2019.azurecr.io/authapplication:stable
---
+++ src/NotesApplication/Controllers/Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ACMTTU.NoteSharing.Platform.NoteService.Controllers
+{
+    [Route("api/notes")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        [HttpGet("new")]
+        public ActionResult<string> GetNewValues()
+        {
+            return "Hello World!";
+        }
+    }
+}
