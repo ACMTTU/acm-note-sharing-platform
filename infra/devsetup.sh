5bcf8d2f5361f2271a53c136a0e654170242a851
ynigoreyes <ynigoreyes@gmail.com>
2019-08-16 14:26:34 -0500

    Adding scripts to add users to Azure AD

--- infra/devsetup.sh
+++ infra/devsetup.sh
@@ -1,7 +1,10 @@
 az login
 
+echo 'Resource Group Name: '
+read resourceGroupName
+
 # Set up their dev space
-echo 'Please enter your name: '
+echo 'Please enter your name (alphanumeric, no spaces): '
 read name
 
-az aks use-dev-spaces -g acm -n acm-aks -y -s $name
+az aks use-dev-spaces -g $resourceGroupName -n acm-aks -y -s dev/$name
