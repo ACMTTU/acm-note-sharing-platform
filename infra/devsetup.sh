3d865859a0e1d58ee381a0cb7900c8807ece4036
ynigoreyes <ynigoreyes@gmail.com>
2019-08-21 17:44:19 -0500

    fixing up pipelines to support on-change builds

--- infra/devsetup.sh
+++ infra/devsetup.sh
@@ -1,10 +1,7 @@
 az login
 
-echo 'Resource Group Name: '
-read resourceGroupName
-
 # Set up their dev space
 echo 'Please enter your name (alphanumeric, no spaces): '
 read name
 
-az aks use-dev-spaces -g $resourceGroupName -n acm-aks -y -s dev/$name
+az aks use-dev-spaces -g acm-notes -n acm-aks -y -s dev/$name
--- infra/resources/deploy-resources.sh
+++ infra/resources/deploy-resources.sh
@@ -1,7 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build $DIR/../. -t acmnotesregistry.azurecr.io/catalogapplication:stable
-docker push acmnotesregistry.azurecr.io/catalogapplication:stable
+echo 'Resource Group Name: '
 read resourceGroupName
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -12,27 +11,13 @@ az group deployment create -g $resourceGroupName --template-file $DIR/azuredeplo
 # Turn on Dev Spaces
 az aks use-dev-spaces -g $resourceGroupName -n acm-notes-aks -s dev -y
 
+# Install Helm inside AKS
 kubectl create serviceaccount tiller --namespace kube-system
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller --history-max 200
 
-# Allow AKS to talk to ACR by creating a role assignment
-CLIENT_ID=$(az aks show --resource-group $resourceGroupName --name acm-notes-aks --query "servicePrincipalProfile.clientId" --output tsv)
-ACR_ID=$(az acr show --name acmnotesregistry --resource-group $resourceGroupName --query "id" --output tsv)
-az role assignment create --assignee $CLIENT_ID --role acrpull --scope $ACR_ID
-read resourceGroupName
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# Deploy Redis, AKS/Kubernetes, CosmosDB, Storage, EventHub, and ACR
-az group deployment create -g $resourceGroupName --template-file $DIR/azuredeploy.json
-
-# Turn on Dev Spaces
-az aks use-dev-spaces -g $resourceGroupName -n acm-notes-aks -s dev -y
-
-kubectl create serviceaccount tiller --namespace kube-system
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller --history-max 200
+# Create a production namespace to hold the production services
+kubectl create ns production
 
 # Allow AKS to talk to ACR by creating a role assignment
 CLIENT_ID=$(az aks show --resource-group $resourceGroupName --name acm-notes-aks --query "servicePrincipalProfile.clientId" --output tsv)
