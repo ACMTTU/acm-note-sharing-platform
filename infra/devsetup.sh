e3debcace51b78f3066b1c21bd344531aa3a7279
ynigoreyes <ynigoreyes@gmail.com>
2019-08-23 03:58:41 -0500

    fixing name

--- infra/devsetup.sh
+++ infra/devsetup.sh
@@ -4,4 +4,4 @@ az login
 echo 'Please enter your name (alphanumeric, no spaces): '
 read name
 
-az aks use-dev-spaces -g acm-notes -n acm-aks -y -s dev/$name
+az aks use-dev-spaces -g acm-notes -n acm-notes-aks -y -s dev/$name
