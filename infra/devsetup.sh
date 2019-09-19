90300ef9269be80fce97ad252ed86b0908acfe77
ynigoreyes <ynigoreyes@gmail.com>
2019-09-19 08:49:26 -0500

    setting up keyvault for cluster

--- infra/devsetup.sh
+++ infra/devsetup.sh
@@ -5,3 +5,36 @@ echo 'Please enter your name (alphanumeric, no spaces, all lowercase): '
 read name
 
 az aks use-dev-spaces -g acm-notes -n acm-notes-aks -y -s dev/$name
+
+# Migrate Secrets from the default namespace to the namespace you created
+secrets=$(echo "$(kubectl get secrets acmkvcreds --namespace=default -oyaml)" | sed "s/default/$name/g")
+echo $secrets | kubectl apply --namespace miggyreyes -f -
+
+
+# Windows
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ];
+then
+    az.cmd login
+    
+    # Set up their dev space
+    echo 'Please enter your name (alphanumeric, no spaces, all lowercase): '
+    read name
+    
+    az.cmd aks use-dev-spaces -g acm-notes -n acm-notes-aks -y -s dev/$name
+    
+    # Migrate Secrets from the default namespace to the namespace you created
+    secrets=$(echo "$(kubectl get secrets acmkvcreds --namespace=default -oyaml)" | sed "s/default/$name/g")
+    echo $secrets | kubectl apply --namespace miggyreyes -f -
+else
+    az login
+    
+    # Set up their dev space
+    echo 'Please enter your name (alphanumeric, no spaces, all lowercase): '
+    read name
+    
+    az aks use-dev-spaces -g acm-notes -n acm-notes-aks -y -s dev/$name
+    
+    # Migrate Secrets from the default namespace to the namespace you created
+    secrets=$(echo "$(kubectl get secrets acmkvcreds --namespace=default -oyaml)" | sed "s/default/$name/g")
+    echo $secrets | kubectl apply --namespace miggyreyes -f -
+fi
