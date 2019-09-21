3211f4ef66bb5aef09d10a384c6e0cd40286fcb3
ynigoreyes <ynigoreyes@gmail.com>
2019-09-21 17:20:13 -0500

    adding scripts to populate keyvault

--- infra/devsetup.sh
+++ infra/devsetup.sh
@@ -1,16 +1,3 @@
-az login
-
-# Set up their dev space
-echo 'Please enter your name (alphanumeric, no spaces, all lowercase): '
-read name
-
-az aks use-dev-spaces -g acm-notes -n acm-notes-aks -y -s dev/$name
-
-# Migrate Secrets from the default namespace to the namespace you created
-secrets=$(echo "$(kubectl get secrets acmkvcreds --namespace=default -oyaml)" | sed "s/default/$name/g")
-echo $secrets | kubectl apply --namespace miggyreyes -f -
-
-
 # Windows
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ];
 then
