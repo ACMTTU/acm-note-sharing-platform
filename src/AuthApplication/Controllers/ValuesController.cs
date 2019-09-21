8bb396c20f6300b2cebae948d86eb84484dcb85d
ynigoreyes <ynigoreyes@gmail.com>
2019-09-21 17:40:34 -0500

    new minor version with proper build

--- src/AuthApplication/Controllers/ValuesController.cs
+++ src/AuthApplication/Controllers/ValuesController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ACMTTU.NoteSharing.Platform.AuthApplication.Controllers
