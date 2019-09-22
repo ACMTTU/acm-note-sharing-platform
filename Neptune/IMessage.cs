4e42468b38e0e0cf52d058af73452d05607cd257
zachary lefevre <zachlefevre@gmail.com>
2019-09-22 15:24:40 -0500

    style fix imessage

--- Neptune/IMessage.cs
+++ Neptune/IMessage.cs
@@ -5,27 +5,22 @@ namespace Neptune
     public interface IRequest<TService>
         where TService : IService
     { }
-
     public interface IGetRequest<TService> : IRequest<TService>
         where TService : IService
     { }
     public interface IPostRequest<TService> : IRequest<TService>
     where TService : IService
     { }
-
     public interface IPutRequest<TService> : IRequest<TService>
     where TService : IService
     { }
     public interface IDeleteRequest<TService> : IRequest<TService>
     where TService : IService
     { }
-
     public interface IResponse<TService, TMessage>
         where TMessage : IRequest<TService>
         where TService : IService
     {
         Object contents();
     }
-
-
 }
