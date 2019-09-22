351bdc5c427bb3edb6d62180ee0cb65496c0487f
zachary lefevre <zachlefevre@gmail.com>
2019-09-22 15:06:04 -0500

    including put and delete http verbs in comm lib

--- Neptune/IMessage.cs
+++ Neptune/IMessage.cs
@@ -12,6 +12,14 @@ namespace Neptune
     public interface IPostRequest<TService> : IRequest<TService>
     where TService : IService
     { }
+
+    public interface IPutRequest<TService> : IRequest<TService>
+    where TService : IService
+    { }
+    public interface IDeleteRequest<TService> : IRequest<TService>
+    where TService : IService
+    { }
+
     public interface IResponse<TService, TMessage>
         where TMessage : IRequest<TService>
         where TService : IService
