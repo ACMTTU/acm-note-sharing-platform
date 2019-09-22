a3207328d7faf186c4ce4407ae3be37da7d1df37
zachary lefevre <zachlefevre@gmail.com>
2019-09-22 15:23:33 -0500

    style fix iservice

--- Neptune/Services/IService.cs
+++ Neptune/Services/IService.cs
@@ -8,33 +8,28 @@ namespace Neptune
         where TService : IService
     {
         private HttpClient client;
-
         public MessageService(HttpClient client)
         {
             this.client = client;
         }
-
         public TResponse GetRequest<TMessage, TResponse>(TMessage msg)
             where TMessage : IGetRequest<TService>
             where TResponse: IResponse<TService, TMessage>
         {
             throw new NotImplementedException();
         }
-
         public TResponse PostRequest<TMessage, TResponse>(TMessage msg)
             where TMessage : IPostRequest<TService>
             where TResponse : IResponse<TService, TMessage>
         {
             throw new NotImplementedException();
         }
-
         public TResponse PutRequest<TMessage, TResponse>(TMessage msg)
             where TMessage : IPutRequest<TService>
             where TResponse : IResponse<TService, TMessage>
         {
             throw new NotImplementedException();
         }
-
         public TResponse DeleteRequest<TMessage, TResponse>(TMessage msg)
             where TMessage : IDeleteRequest<TService>
             where TResponse : IResponse<TService, TMessage>
