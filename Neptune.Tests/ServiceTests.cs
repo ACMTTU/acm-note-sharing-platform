75c600628bcf241583ac349b3fc97215b061ec49
zachary lefevre <zachlefevre@gmail.com>
2019-09-22 17:18:17 -0500

    Added test using mock factory

---
+++ Neptune.Tests/ServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Neptune.Tests
+{
+    public class FakeHttpMessageHandler : DelegatingHandler
+    {
+        private HttpResponseMessage _fakeResponse;
+        public FakeHttpMessageHandler(HttpResponseMessage responseMessage)
+        {
+            _fakeResponse = responseMessage;
+        }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(_fakeResponse);
+        }
+    }
+    public interface IFakeService : IService { }
+    public class FakeApp : MessageService<IFakeService>
+    {
+        public FakeApp(System.Net.Http.IHttpClientFactory clientFactory) : base(clientFactory) { }
+    }
+    public class FakeGetRequest : GetRequest<IFakeService>
+    {
+        public override Uri Uri => new Uri("http://do.notclick.me");
+    }
+    public class FakeGetResponse : Response<IFakeService, FakeGetRequest>
+    {
+        public string body = "default body";
+        public string errorCode = "No error... because I am fake";
+
+        public FakeGetResponse()
+        {
+        }
+
+        public FakeGetResponse(string body)
+        {
+            this.body = body;
+        }
+    }
+
+    [TestFixture]
+    class ServiceTests
+    {
+        [Test]
+        public void WhenAReqIsProvided_ServiceShouldReturnCorrectResp()
+        {
+            var fakeResponse = new FakeGetResponse("I am fake");
+            var fakeMessageHandler = new FakeHttpMessageHandler(new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(JsonConvert.SerializeObject(fakeResponse),
+                Encoding.UTF8,
+                "application/json")
+            });
+            var fakeClient = new HttpClient(fakeMessageHandler);
+
+            var mockClientFactory = Substitute.For<IHttpClientFactory>();
+            mockClientFactory.CreateClient().Returns(fakeClient);
+
+            var app = new FakeApp(mockClientFactory);
+
+            //Act
+            FakeGetResponse resp = app
+                .GetRequest<FakeGetRequest, FakeGetResponse>(new FakeGetRequest()).Result;
+            Assert.True(resp.body == "I am fake");
+        }
+    }
+}
