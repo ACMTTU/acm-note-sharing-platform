71d631e8618967e3b7d9072a4208affcdb1bd3c6
zachary lefevre <zachlefevre@gmail.com>
2019-09-22 16:59:31 -0500

    using clientFactory transiently

--- src/Shared/Neptune/Services/IService.cs
+++ src/Shared/Neptune/Services/IService.cs
@@ -9,50 +9,62 @@ namespace Neptune
     public abstract class MessageService<TService>
         where TService : IService
     {
-        private readonly HttpClient client;
+        private readonly IHttpClientFactory clientFactory;
         public MessageService(IHttpClientFactory clientFactory)
         {
-            this.client = clientFactory.CreateClient();
+            this.clientFactory = clientFactory;
         }
         public async Task<TResponse> GetRequest<TMessage, TResponse>(TMessage msg)
             where TMessage : GetRequest<TService>
             where TResponse : Response<TService, TMessage>
         {
-            var response = await client.GetAsync(msg.Uri.ToString());
-            response.EnsureSuccessStatusCode();
-            var result = await response.Content.ReadAsStringAsync();
-            TResponse r = JsonConvert.DeserializeObject<TResponse>(result);
-            return r;
+            using (HttpClient client = clientFactory.CreateClient())
+            {
+                var response = await client.GetAsync(msg.Uri.ToString());
+                response.EnsureSuccessStatusCode();
+                var result = await response.Content.ReadAsStringAsync();
+                TResponse r = JsonConvert.DeserializeObject<TResponse>(result);
+                return r;
+            }
         }
         public async Task<TResponse> PostRequest<TMessage, TResponse>(TMessage msg)
             where TMessage : PostRequest<TService>
             where TResponse : Response<TService, TMessage>
         {
-            var response = await client.PostAsync(msg.Uri.ToString(), msg.content);
-            response.EnsureSuccessStatusCode();
-            var result = await response.Content.ReadAsStringAsync();
-            TResponse r = JsonConvert.DeserializeObject<TResponse>(result);
-            return r;
+            using (HttpClient client = clientFactory.CreateClient())
+            {
+                var response = await client.PostAsync(msg.Uri.ToString(), msg.content);
+                response.EnsureSuccessStatusCode();
+                var result = await response.Content.ReadAsStringAsync();
+                TResponse r = JsonConvert.DeserializeObject<TResponse>(result);
+                return r;
+            }
         }
         public async Task<TResponse> PutRequest<TMessage, TResponse>(TMessage msg)
             where TMessage : PutRequest<TService>
             where TResponse : Response<TService, TMessage>
         {
-            var response = await client.PutAsync(msg.Uri.ToString(), msg.content);
-            response.EnsureSuccessStatusCode();
-            var result = await response.Content.ReadAsStringAsync();
-            TResponse r = JsonConvert.DeserializeObject<TResponse>(result);
-            return r;
+            using (HttpClient client = clientFactory.CreateClient())
+            {
+                var response = await client.PutAsync(msg.Uri.ToString(), msg.content);
+                response.EnsureSuccessStatusCode();
+                var result = await response.Content.ReadAsStringAsync();
+                TResponse r = JsonConvert.DeserializeObject<TResponse>(result);
+                return r;
+            }
         }
         public async Task<TResponse> DeleteRequest<TMessage, TResponse>(TMessage msg)
             where TMessage : DeleteRequest<TService>
             where TResponse : Response<TService, TMessage>
         {
-            var response = await client.DeleteAsync(msg.Uri.ToString());
-            response.EnsureSuccessStatusCode();
-            var result = await response.Content.ReadAsStringAsync();
-            TResponse r = JsonConvert.DeserializeObject<TResponse>(result);
-            return r;
+            using (HttpClient client = clientFactory.CreateClient())
+            {
+                var response = await client.DeleteAsync(msg.Uri.ToString());
+                response.EnsureSuccessStatusCode();
+                var result = await response.Content.ReadAsStringAsync();
+                TResponse r = JsonConvert.DeserializeObject<TResponse>(result);
+                return r;
+            }
         }
     }
 }
