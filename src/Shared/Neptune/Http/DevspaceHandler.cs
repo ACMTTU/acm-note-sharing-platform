c9a84b16aedca80341a9f558252ed3866b9706e9
zachary lefevre <zachlefevre@gmail.com>
2019-09-24 14:20:59 -0500

    Creating delgationHandler to ensure azds-route-as is set

---
+++ src/Shared/Neptune/Http/DevspaceHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Neptune.Http
+{
+    class DevspaceHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!request.Headers.Contains("azds-route-as"))
+            {
+                return new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("You must supply the azure dev space azds-route-as header")
+                };
+            }
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
