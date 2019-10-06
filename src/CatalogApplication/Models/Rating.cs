7e96b95e6b7b82ad9e255c4ad7755402faeba679
Urbana <ryankelley95@gmail.com>
2019-10-06 16:05:51 -0500

    Add CatalogApplication initial C# models for Tag/Rating data

--- src/CatalogApplication/Models/Rating.cs
+++ src/CatalogApplication/Models/Rating.cs
@@ -2,6 +2,8 @@ namespace CatalogApplication.Models
 {
     public class Rating
     {
-        
+        public int noteId { get; set; }
+        public double rating { get; set; }
+        public int numRatings { get; set; }
     }
 }
--- src/CatalogApplication/Models/Tags.cs
+++ src/CatalogApplication/Models/Tags.cs
@@ -2,6 +2,12 @@ namespace CatalogApplication.Models
 {
     public class CatalogData
     {
-        
+        public string id { get; set; }
+        public string name { get; set; }
+        public int color { get; set; }
+        public int noteId { get; set; }
+        public bool hidden { get; set; }
+        public bool isSystem { get; set; }
+        public string category { get; set; }
     }
 }
