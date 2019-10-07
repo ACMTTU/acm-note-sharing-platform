fef8e05935c354a6875e73d752290128fd5444d3
Ryan Kelley <ryan.kelley@ttu.edu>
2019-10-06 19:20:32 -0500

    Add CatalogApplication initial C# models for Tag/Rating data (#18)
    * Add CatalogApplication initial C# models for Tag/Rating data
    
    * Added descriptions for each attribute and changed class name to something more descriptive

--- src/CatalogApplication/Models/Tags.cs
+++ src/CatalogApplication/Models/Tags.cs
@@ -1,7 +1,20 @@
 namespace CatalogApplication.Models
 {
-    public class CatalogData
+    public class Tag
     {
-        
+        // Unique ID of the tag, an incremental counter
+        public string id { get; set; }
+        // Tag name, will be normalized to all lowercase and hyphen delimeted
+        public string name { get; set; }
+        // Color associated with the tag, defaults to the hash of name as RGB
+        public int color { get; set; }
+        // The note ID associated with this tag
+        public int noteId { get; set; }
+        // Whether the tag should be hidden from the user
+        public bool hidden { get; set; }
+        // Whether the tag was automatically added by the catalog system
+        public bool isSystem { get; set; }
+        // The category of the tag, i.e. Engineering, Computer Science, Business, etc.
+        public string category { get; set; }
     }
 }
