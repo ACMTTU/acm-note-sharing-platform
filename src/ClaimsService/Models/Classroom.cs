77319f57b86bb60a8da29269bb408e084ff73a14
Atharva Raibagi <atharvaraibagi@gmail.com>
2019-10-14 11:39:18 -0500

    Adding models for Classroom and Notes for claims

---
+++ src/ClaimsService/Models/Classroom.cs
@@ -0,0 +1,34 @@
+namespace ClaimsService.Models
+{
+    public class Classroom
+    {
+        // The classroom id associated with this specific classroom
+        public string classroomId { get; set; }
+
+        // The id of the associated user
+        public string userId { get; set; }
+
+        // Permission to Add User
+        public bool addUser { get; set; }
+
+        // Permission to leave the classroom
+        public bool leave { get; set; }
+
+        // Permission to create a note
+        public bool createNote { get; set; }
+
+        // Permission to block a specific user
+        public bool block { get; set; }
+
+        // Permission to delete a specific user
+        public bool delete { get; set; }
+
+        // Permission to add another Admin
+        public bool addAdmin { get; set; }
+
+        // Check whether the user has been blocked or is still in Active status
+        public bool userStatus { get; set; }
+
+
+    }
+}
