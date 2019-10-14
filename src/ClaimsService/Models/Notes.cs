006b897def1ea411103aecb437919ce1d1c328ff
Atharva Raibagi <atharvaraibagi@gmail.com>
2019-10-14 11:39:18 -0500

    Adding models for Classroom and Notes for claims

---
+++ src/ClaimsService/Models/Notes.cs
@@ -0,0 +1,21 @@
+namespace ClaimsService.Models
+{
+    public class Notes
+    {
+        // The note id associated with this specific note
+        public string noteId { get; set; }
+
+        // The id of the associated user
+        public string userId { get; set; }
+
+        // Permission to watch the note
+        public bool watch { get; set; }
+
+        // Permission to edit the note
+        public bool edit { get; set; }
+
+        // Permission to delete the note
+        public bool delete { get; set; }
+
+    }
+}
