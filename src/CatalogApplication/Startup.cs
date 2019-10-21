4d7307e5f2383894fa069d3ebc0f0db47c3e6002
Urbana <ryankelley95@gmail.com>
2019-10-21 17:16:59 -0500

    Add service to controller

--- src/CatalogApplication/Startup.cs
+++ src/CatalogApplication/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-
+using CatalogApplication.Services;
 namespace ACMTTU.NoteSharing.Platform.CatalogApplication
 {
     public class Startup
@@ -42,6 +42,8 @@ namespace ACMTTU.NoteSharing.Platform.CatalogApplication
             });
 
             services.AddHttpClient();
+
+            services.AddSingleton<CatalogDatabaseService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
