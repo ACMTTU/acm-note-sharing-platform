b293515c6e59853a6535bfad4188799e2bb62a29
Haroon Saeed <44040174+hsaeed20@users.noreply.github.com>
2019-10-21 18:32:21 -0500

    Added models for user application (#28)
    * Added models for user application schema
    
    * Changed the names of the attributes to lowercase so that CosmosDB doesn't get mad.

---
+++ src/UserApplication/Models/UserInfo.cs
@@ -0,0 +1,23 @@
+using Newtonsoft.Json;
+namespace UserApplication.Models
+{
+    //Class that defines notable info about user
+    public class UserInfo
+    {
+        //Specifies the specific userprofile
+        public string id { get; set; }
+        //Name that people will reference the user by
+        public string username { get; set; }
+        //Used to verify that the user is a [TTU?] student
+        public string email { get; set; }
+        //What year the user is in (Freshman, Sophmore, Junior, Senior, Graduate, PHD)
+        public string classification { get; set; }
+        //What degree the user is pursuing (Ex: Computer Science)
+        public string major { get; set; }
+​
+        //toString that returns the fields in a .json format
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
