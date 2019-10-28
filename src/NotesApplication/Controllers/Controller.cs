c75817b2264c2db3f182bae6205ecee82740d856
Miggy Reyes <ynigoreyes@gmail.com>
2019-10-28 16:18:38 -0500

    using PlatformBaseService to connect

--- src/NotesApplication/Controllers/Controller.cs
+++ src/NotesApplication/Controllers/Controller.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using ACMTTU.NoteSharing.Platform.NotesApplication.Services;
 using ACMTTU.NoteSharing.Shared.SDK.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos;
 
-namespace ACMTTU.NoteSharing.Platform.NotesApplication.Controllers
-{
-    [Route("api/notes")]
+namespace ACMTTU.NoteSharing.Platform.NotesApplication.Controllers {
+    [Route ("api/notes")]
     [ApiController]
-    public class NotesController : PlatformBaseController
-    {
-        public NotesController(IHttpClientFactory factory) : base(factory) { }
+    public class NotesController : PlatformBaseController {
+        private NotesMetaDataService notesMetaDataService;
+
+        /// <summary>
+        /// Notes Controller, Handles all of the stuff for notes
+        /// </summary>
+        /// <param name="clientFactory"> An Http Client Factory</param>
+        /// <param name="notesMetaDataService">Here is a service that I use to connect to the database</param>
+        /// <returns></returns>
+        public NotesController (IHttpClientFactory clientFactory, NotesMetaDataService notesMetaDataService) : base (clientFactory) {
+            this.notesMetaDataService = notesMetaDataService;
+        }
 
         /// <summary>
         /// This is how you document code
@@ -21,15 +30,11 @@ namespace ACMTTU.NoteSharing.Platform.NotesApplication.Controllers
         /// </summary>
         /// <param name="id">Some sort of Id</param>
         /// <returns>An array containing a value determined by the parameter</returns>
-        [HttpGet("{id}")]
-        public async Task<ActionResult<string>> GetValues(string id)
-        {
-            using (CosmosClient dbClient = await this.GetDatatbaseClient())
-            {
-                await dbClient.CreateDatabaseIfNotExistsAsync("NotesApplication");
-            }
-
+        [HttpGet ("{id}")]
+        public ActionResult<string> GetValues (string id) {
+            notesMetaDataService.DoStuff ();
             return $"Id value from URL: {id}";
         }
+
     }
 }
