ae184557cb0d4147b2ca45d092b80a69f19b6fe7
ColeTown <33290898+ColeTown@users.noreply.github.com>
2019-11-02 14:35:33 -0500

    commented out broken code
    will hookup later

--- src/ClassApplication/Models/Classroom.cs
+++ src/ClassApplication/Models/Classroom.cs
@@ -8,9 +8,9 @@ namespace ClassApplication.Models
         public int classID { get; set; }                    //From the user
         public string Name { get; set; }                    //From the user
         public string Description { get; set; }             //From the user
-        public IEnumerable<Note> Notes { get; set; }        //Pointer to other collection - Notes
-        public IEnumerable<string> Filters { get; set; }    //changed from Tag //Pointer to other collection - Catalog
-        public IEnumerable<Student> Students { get; set; }  //Pointer to other collection - Students
+//         public IEnumerable<Note> Notes { get; set; }        //Pointer to other collection - Notes
+//         public IEnumerable<string> Filters { get; set; }    //changed from Tag //Pointer to other collection - Catalog
+//         public IEnumerable<Student> Students { get; set; }  //Pointer to other collection - Students
 
     }
 }
