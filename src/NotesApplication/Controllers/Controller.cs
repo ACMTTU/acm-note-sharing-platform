7af9bfc81fcd1032dc571d0a510dc7e48f445381
Jacob Stahl <50686850+Jacob-Stahl@users.noreply.github.com>
2019-11-03 14:09:14 -0600

    Connecting to Database (#40)
    * added service to connect to database under Services folder
    
    * adding api requests to controller
    
    * Added code to EditNote method, it replaced notes with a given id, but it does not verify that the user id is the same, this will be fixed later
    
    * EditNote method now verifies user id using GetNote method, also made other methods return proper values
    
    * changed silly file names

--- src/NotesApplication/Controllers/Controller.cs
+++ src/NotesApplication/Controllers/Controller.cs
@@ -4,36 +4,20 @@ using System.Threading.Tasks;
 using ACMTTU.NoteSharing.Shared.SDK.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos;
+using ACMTTU.NoteSharing.Platform.NotesApplication.Services;
 using NotesApplication;
 
-namespace ACMTTU.NoteSharing.Platform.NotesApplication.Controllers {
-    [Route ("api/notes")]
+namespace ACMTTU.NoteSharing.Platform.NotesApplication.Controllers
+{
+    [Route("api/notes")]
     [ApiController]
-    public class NotesController : PlatformBaseController {
-        /// <summary>
-        /// Notes Controller, Handles all of the stuff for notes
-        /// </summary>
-        /// <param name="clientFactory"> An Http Client Factory</param>
-        /// <returns></returns>
-        public NotesController (IHttpClientFactory clientFactory) : base (clientFactory) {
-        }
 
-        /// <summary>
-        /// After checking if a user has permissions to delete a note 
-        /// deletes the note and all meta data assosiated with note 
-        /// </summary>
-        /// <param name="noteId">The note we are trying to delete</param>
-        /// <param name="userId">The user trying to delete the note</param>
-        /// <returns>if note exists and user has permision to delete return Sucess else return failure</returns>
-        [HttpDelete("{noteId}/users/${userId}")]
-        public async Task DeleteNote(string noteId, string userId)
+    public class NotesController : PlatformBaseController
+    {
+        private Container NotesContainer;
+        public NotesController(IHttpClientFactory factory, DBService db) : base(factory)
         {
-            using (CosmosClient dbClient = await this.GetDatatbaseClient())
-            {
-                var dbRef = await dbClient.CreateDatabaseIfNotExistsAsync("NotesApplication");
-                var containerRef = await dbRef.Database.CreateContainerIfNotExistsAsync("Notes", "/Name");
-            }
-
+            NotesContainer = db.container;
         }
 
         /// <summary>
@@ -45,20 +29,19 @@ namespace ACMTTU.NoteSharing.Platform.NotesApplication.Controllers {
         /// <param name="update">A partial object of the type Note, with the changes</param>
         /// <returns>if note exists and user has permision to edit return Sucess else return failure.</returns>
         [HttpPut("{noteId}/users/{userId}")]
-        public async Task EditNote(string noteId, string userId, Note update)
+        public async Task<bool> EditNote(string noteId, string userId, Note update)
         {
+            Note oldNote = await GetNote(noteId, userId);
 
-        }
-
-        /// <summary>
-        /// get all notes associated with user
-        /// </summary>
-        /// <param name="user_id"></param>
-        /// <returns>if user exists return Sucess else return failure.</returns>
-        [HttpGet("users/{user_id}")]
-        public async Task GetUsersNotes(string user_id)
-        {
-
+            if (oldNote.ownerId == userId) // verifies userId is allowed to modify noteId
+            {
+                await NotesContainer.ReplaceItemAsync<Note>(update, noteId);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         /// <summary>
@@ -67,9 +50,8 @@ namespace ACMTTU.NoteSharing.Platform.NotesApplication.Controllers {
         /// <param name="noteId">The id of the not we want</param>
         /// <returns>if note exists and user has permision to get it return Sucess else return failure.</returns>
         [HttpGet("{noteId}/users/${userId}")]
-        public async Task GetNote(string noteId, string userId)
+        public async Task<Note> GetNote(string noteId, string userId)
         {
-
         }
     }
 
