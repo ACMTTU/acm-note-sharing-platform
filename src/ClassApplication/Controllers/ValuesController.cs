c16b3aebab087b07fca54b69f474a331be7664a9
Cole Townsend <c4townsend@gmail.com>
2019-11-05 19:48:03 -0600

    Added API Controller to add a user to Classroom

--- src/ClassApplication/Controllers/ValuesController.cs
+++ src/ClassApplication/Controllers/ValuesController.cs
@@ -61,5 +61,35 @@ namespace ACMTTU.NoteSharing.Platform.ClassApplication.Controllers
         {
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// This is how you document code
+        /// 
+        /// Visit the microservice's endpoint and append /swagger
+        /// to see your docs in action
+        /// </summary>
+        /// <param name="id">classroom id</param>
+        /// <returns>An array containing a value determined by the parameter</returns>
+        [HttpPut("{id}")]
+        public async Task<ActionResult<string>> UpdateClassroom(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// This is how you document code
+        /// 
+        /// Visit the microservice's endpoint and append /swagger
+        /// to see your docs in action
+        /// </summary>
+        /// <param name="classID">classroom id</param>
+        ///<param name="userID">user id</param>
+        /// <returns>An array containing a value determined by the parameter</returns>
+        [HttpPut("{classID}/Users/{userID}")]
+        public async Task<ActionResult<string>> AddUserToClass(string classID, string userID)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
