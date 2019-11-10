dfe329bdd1a48198a49601bfe7d40cce14c5a0fd
Urbana <ryankelley95@gmail.com>
2019-11-10 15:36:43 -0600

    Add api contract swagger descriptions

--- src/CatalogApplication/Controllers/TagController.cs
+++ src/CatalogApplication/Controllers/TagController.cs
@@ -71,18 +71,21 @@ namespace ACMTTU.NoteSharing.Platform.CatalogApplication.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Updates a tag based off noteId, userId, and name.
         /// </summary>
-        /// <param name=""></param>
+        /// <param name="noteId">The noteId to look for tags in</param>
+        /// <param name="userId">The userId the tag must have</param>
+        /// <param name="name">The tag name to update</param>
         /// <returns></returns>
-        [HttpPut("{noteId}/users/{userId}")]
-        public Task<bool> UpdateTag(string noteId, string userId, Tag update)
+        [HttpPut("{noteId}/users/{userId}/names/{name}")]
+        public Task<bool> UpdateTag(string noteId, string userId, string name, Tag update)
         {
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// 
+        /// Deletes all the tags associated with a specific noteId. Useful for when a note
+        /// is deleted.
         /// </summary>
         /// <param name="noteId">The Note ID associated with the tag to be deleted</param>
         /// <returns>Returns a list of tags that have been deleted</returns>
