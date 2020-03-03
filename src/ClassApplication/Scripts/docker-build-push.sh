e0eeb8a5d02c6a5a446d43424fdde8bb667d5111
Miggy Reyes <ynigoreyes@gmail.com>
2020-03-03 17:16:10 -0600

    Fixing pipelines

--- src/ClassApplication/Scripts/docker-build-push.sh
+++ src/ClassApplication/Scripts/docker-build-push.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build $DIR/../. -t acmnotesregistry.azurecr.io/classapplication:stable
-docker push acmnotesregistry.azurecr.io/classapplication:stable
+docker build $DIR/../. -t notesappregistry.azurecr.io/classapplication:stable
+docker push notesappregistry.azurecr.io/classapplication:stable
