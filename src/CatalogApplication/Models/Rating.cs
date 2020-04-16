f9112856ec5e180352d11a96cd9b9de4eacdc2dd
rkelley20 <ryan.kelley@ttu.edu>
2020-04-16 16:18:21 -0500

    Add a rating DTO object since we do not want API users to specify the unique id we generate

--- src/CatalogApplication/Models/Rating.cs
+++ src/CatalogApplication/Models/Rating.cs
@@ -1,12 +1,23 @@
 namespace CatalogApplication.Models
 {
-    public class Rating
+    public class RatingDto
     {
+        // The user that owns the rating
+        public string userId { get; set; }
         // The note ID associated with this rating
         public string noteId { get; set; }
         // The running average rating for the note
         public double rating { get; set; }
-        // The number of ratings total for the note
-        public int numRatings { get; set; }
+    }
+
+    public class Rating
+    {
+        public string id { get; set; }
+         // The user that owns the rating
+        public string userId { get; set; }
+        // The note ID associated with this rating
+        public string noteId { get; set; }
+        // The running average rating for the note
+        public double rating { get; set; }
     }
 }
