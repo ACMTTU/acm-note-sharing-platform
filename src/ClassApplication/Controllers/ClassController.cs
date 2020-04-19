4eca64aed3b79871c43e4c6728ed3535aabeb77e
anamol222@gmail.com <Mynepal101?>
2020-04-18 22:10:34 -0500

    I added three methods GetClassroomByID, GetClassroomByName, and DeleteClass. I apologize for any errors, and looking for suggestions if needed

--- src/ClassApplication/Controllers/ClassController.cs
+++ src/ClassApplication/Controllers/ClassController.cs
@@ -32,6 +32,7 @@ namespace ACMTTU.NoteSharing.Platform.ClassApplication.Controllers
             throw new NotImplementedException();
         }
 
+
         /// <summary>
         /// 
         /// Returns data from a classroom from a given classroom ID
@@ -40,11 +41,88 @@ namespace ACMTTU.NoteSharing.Platform.ClassApplication.Controllers
         /// <param name="id">classroom id</param>
         /// <returns>An array containing a value determined by the parameter</returns>
         [HttpGet("GetClassByID/{id}")]
-        public async Task<ActionResult<string>> GetClassroom(string id)
+        /*
+                public async Task<ActionResult<string>> GetClassroom(string id)
+                {
+                    //to check if i am pushing it right
+                    //throw new NotImplementedException();
+         }
+         */
+        public async Task<ActionResult<string>> GetClassroomByID(string id)
         {
-            //to check if i am pushing it right
-            throw new NotImplementedException();
+
+
+            string Text = " SELECT * FROM c WHERE c.noteId =@id";
+            QueryDefinition query = new QueryDefinition(Text).WithParameter("@Id", id);
+
+            FeedIterator<Classroom> iterator = classesContainer.ReadItemAsync<Classroom>(query);
+
+            List<Classroom> classroomId = new List<Classroom>(classId);
+
+            //this is an example to add class
+            classroomId.Add("CS 3364"); //example
+            classroomId.Add(new (ClassController) { GetClassroom = "CS 3375" });
+            var classId = id;
+
+            while (iterator.HasMoreResults)
+            {
+                FeedResponse<Classroom> result = await iterator.ReadNextAsync();
+                foreach (Classroom classId in result)
+                {
+                    classroomId.Add(classId);
+                }
+            }
+            //Returns data from a classroom from a given classroom ID
+            return classroomId;
+
+
+            if (classroomId == null)
+            {
+                Console.WriteLine("No any Classes. Click '+' and type new class Id to create one.")
+                // availableClassroom = classroomList;
+            }
+            else
+            {
+                Console.WriteLine("The total number of class room is of different class is: " + classroomId.count +
+                "Please select your intended class.");
+                //display the class room with respective classId
+                var classId = classroomId.classId;
+            }
         }
+
+
+        public async Task<ActionResult<string>> GetClassroomByName(string className)
+        {
+
+
+            string Text = " SELECT * FROM c WHERE c.name= @name =@className";
+            QueryDefinition query = new QueryDefinition(Text).WithParameter("@className", className);
+
+            FeedIterator<Classroom> iterator = classesContainer.ReadItemAsync<Classroom>(query);
+
+            List<string> classroomName = new List<string>();
+
+            //this is an example to add class
+            classroomName.Add("Computer Architecture"); //example
+            classroomName.Add(new (ClassController) { GetClassroom = "Software Engineering" });
+            var classId = id;
+
+            while (iterator.HasMoreResults)
+            {
+                FeedResponse<Classroom> result = await iterator.ReadNextAsync();
+                foreach (Classroom className in result)
+                {
+                    classroomName.Add(className);
+                }
+            }
+            //Returns data from a classroom from a given classroom ID
+            return classroomName;
+
+        }
+
+
+
+
 
         /// <summary>
         /// 
@@ -89,34 +167,59 @@ namespace ACMTTU.NoteSharing.Platform.ClassApplication.Controllers
         ///</summary>
         ///<param name= "classId">ID of the classroom </param>
         [HttpDelete("{classId}")]
-        public async Task<ActionResult<string>> DeleteClass(string classId)
-        {
-            throw new NotImplementedException();
-        }
 
-        /// <summary>
-        ///This call is used to update the name and/or description of a class
-        ///by classId
-        ///</summary>
-        ///<param name="classId">Id of the classroom</param>
-        ///<param name="className">Name of classroom to set</param>
-        ///<param name="classDescription">Description of classroom to set</param>
-        [HttpPut("{classId}")]
-        public async Task<ActionResult<string>> UpdateClass(string classId, string className, string classDescription)
-        {
-            throw new NotImplementedException();
-        }
+        /*        public async Task<ActionResult<string>> DeleteClass(string classId)
+                {
+                    throw new NotImplementedException();
+                }
+        */
 
-        ///<summary>
-        ///This call is used to query a list of notes in a class
-        ///by classId
-        ///</summary>
-        ///<param name="classId">Id of the classroom</param>
-        ///<param name="noteName">Name of the note to search for</param>
-        [HttpGet("{classId}")]
-        public async Task<ActionResult<string>> QueryNote(string classId, string noteName)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<ActionResult<string> DeleteClass(string classId)
+            {
+
+            //here, it gets the list of class id's in the classId
+                List<Classroom> id = GetClassroomByID(classId).Result;
+
+                if(!id.Any())
+                return EmptyList("classId does not exist"); //here i am confused where to return if there is no class id
+
+        //find a specific class with the parameter classID
+        Classroom getlass = id.Find(classroom => classroom.classId == classId);
+
+                if(getClass != null)
+                {
+                    await classesContainer.DeleteItemAsync<Classroom>(getClass.id, new PartitionKey(getClass.classId));
+                    return Deleted("Class successfully deleted");
+
+    }
+}
+
+
+
+
+/// <summary>
+///This call is used to update the name and/or description of a class
+///by classId
+///</summary>
+///<param name="classId">Id of the classroom</param>
+///<param name="className">Name of classroom to set</param>
+///<param name="classDescription">Description of classroom to set</param>
+[HttpPut("{classId}")]
+public async Task<ActionResult<string>> UpdateClass(string classId, string className, string classDescription)
+{
+    throw new NotImplementedException();
+}
+
+///<summary>
+///This call is used to query a list of notes in a class
+///by classId
+///</summary>
+///<param name="classId">Id of the classroom</param>
+///<param name="noteName">Name of the note to search for</param>
+[HttpGet("{classId}")]
+public async Task<ActionResult<string>> QueryNote(string classId, string noteName)
+{
+    throw new NotImplementedException();
+}
     }
 }
