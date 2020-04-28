48e83e9fe5925561ff1e6315c8c533288d05664a
John M <44129744+jmanacup@users.noreply.github.com>
2020-04-28 15:56:02 -0500

    ConnectionService.cs created (#82)
    * ConnectionService.cs created
    
    * ConnectionService fixed. DeleteUser also updated
    
    * Fixed createNewUser
    
    * CreateUser function name

---
+++ src/UserApplication/Services/ConnectionService.cs
@@ -0,0 +1,32 @@
+using ACMTTU.NoteSharing.Shared.SDK.Services;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace ACMTTU.NoteSharing.Platform.UserApplication.Services
+{
+    public class ConnectionService : PlatformBaseService
+    {
+
+        public Container userContainer { get; private set; }
+
+        public ConnectionService(IHttpClientFactory clientFactory) : base(clientFactory) { }
+
+        public override Task Setup()
+        {
+            this._setUpDatabase().Wait();
+
+            return Task.CompletedTask;
+        }
+
+        private async Task _setUpDatabase()
+        {
+            var databaseReference = await this.dbClient.CreateDatabaseIfNotExistsAsync("UserApplication");
+
+            // You can make multiple containers per database so have fun
+            var userContainerReference = await databaseReference.Database.CreateContainerIfNotExistsAsync("Users", "/id");
+
+            this.userContainer = userContainerReference.Container;
+        }
+    }
+}
