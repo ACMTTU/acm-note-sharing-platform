4adda915a98e71fda268cd6613d90c2b40e6df4e
Garret Carmouche <35586123+GarretCarmouche@users.noreply.github.com>
2020-05-02 17:42:52 -0500

    Update Classroom.cs

--- src/ClassApplication/Models/Classroom.cs
+++ src/ClassApplication/Models/Classroom.cs
@@ -8,9 +8,9 @@ namespace ClassApplication.Models
         public string classID { get; set; }                    //From the user
         public string Name { get; set; }                    //From the user
         public string Description { get; set; }             //From the user
-        public IEnumerable<string> Notes { get; set; }        //id of other collection - Notes
-        public IEnumerable<string> Filters { get; set; }    //changed from Tag //id of other collection - Catalog
-        public IEnumerable<string> Students { get; set; }  //id of other collection - Students
+        public HashSet<string> Notes { get; set; }        //id of other collection - Notes
+        public HashSet<string> Filters { get; set; }    //changed from Tag //id of other collection - Catalog
+        public HashSet<string> Students { get; set; }  //id of other collection - Students
 
 
     }
