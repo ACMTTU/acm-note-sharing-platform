5e2d4d2ccf880555bbd411e6f870da4531e17443
rajeev <rajeev.goonie98@gmail.com>
2020-05-09 00:37:01 -0700

    add constructor and formatted

--- src/ClassApplication/Models/Classroom.cs
+++ src/ClassApplication/Models/Classroom.cs
@@ -13,22 +13,47 @@ namespace ClassApplication.Models
         public HashSet<string> Filters { get; set; }    //changed from Tag //id of other collection - Catalog
         public HashSet<string> Students { get; set; }  //id of other collection - Students
 
+        /// <summary>
+        ///     Classroom Constructor; Required data to create a classroom
+        /// </summary>
+        /// <param name="id">unique id for the classroom</param>
+        /// <param name="ownerID">user id of the person who had full admin privileges</param>
+        /// <param name="name">the name of the classroom</param>
+        /// <param name="description">a description of the classroom</param>
+        public Classroom(string id, string ownerID, string name, string description)
+        {
+            this.id = id;
+            this.ownerID = ownerID;
+            this.Name = name;
+            this.Description = description;
+
+            this.Notes = new HashSet<string>();
+            this.Filters = new HashSet<string>();
+            this.Students = new HashSet<string>();
+        }
+
+        /// <summary>
+        ///     Changes the name of the classroom
+        /// </summary>
+        /// <param name="newName">the new name of the classroom</param>
         public void setName(string newName)
         {
             this.Name = newName;
         }
 
         /// <summary>
-        /// Adds note to this classroom's collection of notes
+        ///     Adds note to this classroom's collection of notes
         /// </summary>
         /// <param name="noteID">The id of the note object to be added </param>
         public void AddNote(string noteID)
         {
-
             Notes.Add(noteID);
-
         }
 
+        /// <summary>
+        ///     Changes the description of the classroom
+        /// </summary>
+        /// <param name="newDescription"></param>
         public void setDescription(string newDescription)
         {
             this.Description = newDescription;
@@ -40,12 +65,21 @@ namespace ClassApplication.Models
         /// <param name="removeId">User ID of the user being removed</param>
         public void RemoveStudent(string removeId)
         {
-
             Students.Remove(removeId);
-
         }
 
+        /// <summary>
+        ///     Checks if a user is contained in a class
+        /// </summary>
+        /// <param name="userId">the id of the user being checked</param>
+        /// <returns>if the user is in the classroom</returns>
         public bool ContainsStudent(string userId) { return Students.Contains(userId); }
+
+        /// <summary>
+        ///     Checks if a user is the owner of a class
+        /// </summary>
+        /// <param name="userId">the id of the user being checked</param>
+        /// <returns>if the user is in the classroom</returns>
         public bool StudentIsOwner(string userId) { return ownerID.Equals(userId); }
 
     }
